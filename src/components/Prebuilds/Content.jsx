5cabbe31c7e4ba6fa8be317252cf625a9f4443a9
Matt <mattymeans@gmail.com>
2025-02-26 00:09:52 -0700

    Added Template Selector Skeleton

--- src/components/Prebuilds/Content.jsx
+++ src/components/Prebuilds/Content.jsx
@@ -7,6 +7,22 @@ function Content(){
     return(
         <div>
             <PageType />
+
+        <div>
+            <h1 class="text-[4rem] text-center">Template Selector</h1>
+            <div class="grid grid-cols-5 gap-3 border-5 border-gray-800 rounded-2xl p-2 w-[90vw] place-self-center bg-green-100">
+                <div class="border-double border-10 border-gray-600 col-span-3 col-start-2 bg-white">
+                    <h2 class="text-[1.5rem]">Number of pages:  </h2>
+                </div>
+                <div class="border-double border-10 border-gray-600 col-start-1 col-span-1 bg-white">
+                    <h2 class="text-[1.5rem] text-center underline">Style Selector</h2>
+                </div>
+                <div class="border-double border-10 border-gray-600 col-span-4 bg-white">
+                    <h2 class="text-[1.5rem] text-center underline">Preview</h2>
+                </div>
+            </div>
+        </div>
+            
       </div>
     )
 }
--- src/components/Prebuilds/PageType.jsx
+++ src/components/Prebuilds/PageType.jsx
@@ -10,9 +10,9 @@ function PageType(){
     }
 
     return(
-        <div class="my-35 flex flex-nowrap flex-col items-center">
-            <div class="flex flex-nowrap justify-between gap-25 mt-10 items-end">
-            <h1 class="text-center text-[2rem] border-double border-5 border-emerald-500 rounded-t-2xl p-4 bg-linear-to-t from-gray-300 to-white">Select a type to see a brief description</h1>
+        <div class="my-35 flex flex-col items-center pb-15 border-double border-b-5 border-t-5">
+            <div class="flex gap-25 mt-10 items-end shrink">
+                <h1 class="text-center text-[2rem] border-double border-5 border-emerald-500 rounded-t-2xl p-4 bg-linear-to-t from-gray-300 to-white">Select a type to see a brief description</h1>
                 <TypeButtons
                     text="Static"
                     onClicked={changeType}
@@ -35,6 +35,12 @@ function PageType(){
                 />
             </div>
             <p class="border-double border-5 border-black w-[80vw] rounded-2xl text-[2rem] p-4 bg-linear-to-b from-white to-gray-300 shadow-[0_12px_12px_10px] shadow-gray-400">{typeDescription}</p>
+            <h2 class="text-center text-[2rem] border-double border-5 border-emerald-500 rounded-b-2xl p-4 bg-linear-to-t from-gray-300 to-white">Scroll down to check out the Template Selector!</h2>
+            <h2 class="w-[50vw] mt-5 text-red-500">** If you open this page on a smartphone or tablet you may notice some aspects of this page adjust to the screen size, while others do not.
+                This is referred to as 'Responsivness', and is implemented to adjust the view of a website to fit any screen size. I intentionally left responsiveness
+                out of some components as a way to show you another aspect of building a web page. Adding responsiveness can increase the amount of time it takes to build,
+                but will allow users to view your web page with no issues across any device or screen.
+            </h2>
             
       </div>
     )
