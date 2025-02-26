188d42aaadab167af38e7efc1f50d936eeaee4dd
Matt <mattymeans@gmail.com>
2025-02-26 00:16:26 -0700

    Fixed a typo

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -8,7 +8,7 @@ function Intro(){
            <NameTag /> 
         </div>
         <div class="basis-1/3 border-double border-5 rounded-full p-15 size-[50%] self-center bg-paper">
-            <p class="text-[2rem] text-center">✨ Having a great website is a crucial step to success. With my reiptoire of knowledge and expertise, I'd love to help you reach that success! ✨ </p>
+            <p class="text-[2rem] text-center">✨ Having a great website is a crucial step to success. With my repertoire of knowledge and expertise, I'd love to help you reach that success! ✨ </p>
         </div>
         
     </div>
