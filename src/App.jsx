7d4c7da3691a67cccc8bb5593efedf467119d034
Matt <mattymeans@gmail.com>
2025-06-14 01:09:26 -0600

    Updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,13 @@ function App() {
   const [NameVisible, changeNameVisible] = useState(false);
 
   function FirstClick(){
-    changeAssetVisible(!assestVisible);
+    if(NameVisible){
+      changeAssetVisible(!assestVisible);
+      changeNameVisible(!NameVisible);
+    } else{
+      changeAssetVisible(!assestVisible);
+    }
+    
   }
 
   function SecondClick(){
@@ -17,29 +23,30 @@ function App() {
   }
 
   return (
-    <div className='p-25 mt-15 place-self-center flex flex-col gap-15 border-15 border-double border-emerald-600 rounded-4xl h-[90vh] w-[90%] bg-gray-200'>
-      <button className='hover:bg-sky-200 active:bg-sky-400' onClick={() => (FirstClick())}>
+    <div className='p-5 md:p-25 mt-15 place-self-center flex flex-wrap gap justify-start border-15 border-double border-emerald-600 rounded-4xl h-[90vh] w-[90%] bg-gray-200'>
+      <button className='hover:bg-sky-200 active:bg-sky-400 self-start basis-full' onClick={() => (FirstClick())}>
         <div className='flex gap-5'>
           <span className='place-self-center'>{assestVisible ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon /> }</span>
-          <img src={Folder}/>
-          <h1 className='place-self-center text-3xl'>Web Development Projects</h1>
+          <img src={Folder} />
+          <h1 className='place-self-center md:text-3xl'>Web Development Projects</h1>
         </div>        
       </button>
 
-      <button className={[`hover:bg-sky-200 active:bg-sky-400 pl-25 ${assestVisible ? 'visible' : 'hidden'}`]} onClick={() => (SecondClick())}>
+      <button className={[`hover:bg-sky-200 active:bg-sky-400 pl-25 ${assestVisible ? 'visible' : 'hidden'} self-start basis-full`]} onClick={() => (SecondClick())}>
         <div className='flex gap-5'>
           <span className='place-self-center'>{NameVisible ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon /> }</span>
           <img src={Folder}/>
-          <h1 className='place-self-center text-3xl'>Assets</h1>
+          <h1 className='place-self-center md:text-3xl'>Assets</h1>
         </div>        
       </button>
 
-      <NavLink to="/home">
-        <div className={[`${NameVisible ? 'visible' : 'hidden'} animate-jump-in animate-once animate-duration-500 animate-ease-in mt-[5%] border-double border-5 p-15 rounded-full pattern`]}>
+      <div className={[`${NameVisible ? 'visible' : 'hidden'} animate-jump-in animate-once animate-duration-500 animate-ease-in border-double border-5 md:p-15 rounded-4xl pattern basis-full`]}>
+          <NavLink to="/home">
           <h1 className='text-center text-7xl mb-15 text-white'>Matthew Means</h1>
           <h2 className='text-center text-4xl text-white'>Full-Stack Web Developer 🌐</h2>
-        </div>
-      </NavLink>
+          </NavLink>
+      </div>
+      
 
     </div>
     
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,10 +4,10 @@ function Home(){
     return(
         <div>
             <Banner/>
-            <div className='animate-fade-left animate-once animate-ease-in mx-[5%] border-10 border-double rounded-3xl border-emerald-600 p-15 bg-gray-200'>
-                <h1 className='text-[8vw]'>Hey there, I'm Matt.</h1>
-                <p className='text-[1.8vw] underline underline-offset-15 decoration-amber-400 mb-10'>✨I'm a full-stack web developer with the skills and experience to help build just about any webpage!</p>
-                <p className="text-3xl leading-15 border-1 p-5 rounded-3xl">On the front-end I can customize a webpage to just about any specification and at the back-end I can write all the necessary code to provide the expected functionality.
+            <div className='animate-fade-left animate-once animate-ease-in mx-[5%] border-10 border-double rounded-3xl border-emerald-600 p-15 bg-gray-200 passion-one-regular'>
+                <h1 className='text-7xl'>Hey there, I'm Matt.</h1>
+                <p className='text-3xl underline underline-offset-15 decoration-amber-400 mb-10'>✨I'm a full-stack web developer with the skills and experience to help build just about any webpage!</p>
+                <p className="md:text-5xl leading-15 border-5 border-double p-5 rounded-3xl ">On the front-end I can customize a webpage to just about any specification and at the back-end I can write all the necessary code to provide the expected functionality.
                     Don't just take my word for it though! Please browse this website I built from scratch to get an idea of what I can do!
                     A list of all my skills as a web developer can be found on the 'Skills' page.
                     Want a little background? Check out the 'About' page.
