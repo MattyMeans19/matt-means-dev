a13b1af2271fd00476a5f52a3c4d051b45b0f918
Matt <mattymeans@gmail.com>
2025-07-02 00:53:27 -0600

    Structural Changes

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -1,56 +1,13 @@
 import React, {useEffect, useState} from "react";
-import DocIcon from "/icons8-document-69.png";
-import SkillsIcon from "/icons8-skills-64.png";
-import PortfolioIcon from "/icons8-portfolio-32.png";
-import CMDIcon from "/icons8-command-64.png";
 import Icon from "./Icon";
 import Window from "./Window";
 import TaskBar from "./TaskBar";
 import TaskBarItem from "./TaskbarItem";
 import Menu from "./Menu";
-
+import { apps } from "./Apps";
 
 
 function Desktop(){
-    const [apps] = useState([
-        {
-            name: "Welcome",
-            icon: CMDIcon,
-            visibility: true,
-            minimized: false,
-            id: 1, 
-            height: "h-[600px]",
-            width: "w-[600px]",
-            isFocus: true        
-        },
-        {
-            name: "About Me",
-            icon: DocIcon,
-            minimized: false,
-            id: 2, 
-            height: "h-[500px]",
-            width: "w-[800px]",
-            isFocus: false        
-        },
-        {
-            name: "Skills",
-            icon: SkillsIcon,
-            minimized: false,
-            id: 3, 
-            height: "h-[800px]",
-            width: "w-[1000px]",
-            isFocus: false        
-        },
-        {
-            name: "Portfolio",
-            icon: PortfolioIcon,
-            minimized: false,
-            id: 4, 
-            height: "h-[800px]",
-            width: "w-[1200px]",
-            isFocus: false        
-        }
-    ])
 
     const [windows, updateWindows] = useState([apps[0]])
 
