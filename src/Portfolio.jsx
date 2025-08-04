30c07a5fef518e780fec898888923d448a2062c1
Matt <mattymeans@gmail.com>
2025-08-04 15:03:53 -0600

    Added first site to portfolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,34 +1,11 @@
 import React, {useState} from "react";
+import { sites } from "./Sites";
 
 function Portfolio(){
-    const [viewArray] = useState([
-        {
-            id: 1, 
-            name: "Site 1",
-            url: "URL for site...",
-            image: "🖼️",
-            description: "Site Description will go here!"
-        },
-        {
-            id: 2, 
-            name: "Site 2",
-            url: "URL for site...",
-            image: "🖼️",
-            description: "Site Description will go here!"
-        },
-        {
-            id: 3, 
-            name: "Site 3",
-            url: "URL for site...",
-            image: "🖼️",
-            description: "Site Description will go here!"
-        }
-        
-    ]);
     const [currentView, changeView] = useState(0);
 
     function nextView(){
-        if(currentView !== (viewArray.length - 1)){
+        if(currentView !== (sites.length - 1)){
             changeView(currentView + 1);
         }
     }
@@ -40,13 +17,13 @@ function Portfolio(){
     }
 
     return(
-        <div className="border-3 border-gray-400 bg-gray-900 size-full flex flex-nowrap gap-10 justify-between place-items-center p-5">
+        <div className="border-3 border-gray-400 bg-gray-900 size-[100%] flex flex-nowrap justify-between place-items-center md:p-5">
             <button className="size-10 active:bg-blue-600" onClick={() => (prevView())}>◀️</button>
-            <div className="basis-full h-full border-5 border-double border-black rounded-4xl flex flex-col gap-30 p-10 bg-cyan-500">
-                <h1 className="text-center text-5xl underline">{viewArray[currentView].name}</h1>
-                <h2 className="text-center text-2xl">{viewArray[currentView].url}</h2>
-                <span className="text-center">{viewArray[currentView].image}</span>
-                <span className="text-start text-3xl">{viewArray[currentView].description}</span>
+            <div className="basis-full h-full border-5 border-double border-black rounded-4xl flex flex-col gap-5 p-10 bg-cyan-500">
+                <h1 className="text-center text-5xl underline">{sites[currentView].name}</h1>
+                <a href={sites[currentView].url} target="_blank" className="text-center text-2xl">{sites[currentView].url}</a>
+                <img src={sites[currentView].image} className="size-full md:size-[70%] place-self-center border-10 border-double"/>
+                <span className="text-center text-3xl">{sites[currentView].description}</span>
             </div>
             <button className="size-10 active:bg-blue-600" onClick={() => (nextView())}>▶️</button>
         </div>
---
+++ src/Sites.js
@@ -0,0 +1,25 @@
+import AKSImage from "/allkeyboardshortcuts.png";
+
+export const sites = [
+    {
+        id: 1, 
+        name: "All Keyboard Shortcuts",
+        url: "https://allkeyboardshortcuts.info",
+        image: AKSImage,
+        description: "A simple webiste that contains lists of keyboard shortcuts for various Operating Systems and Applications!"
+    },
+    {
+        id: 2, 
+        name: "Site 2",
+        url: "URL for site...",
+        image: "🖼️",
+        description: "Site Description will go here!"
+    },
+    {
+        id: 3, 
+        name: "Site 3",
+        url: "URL for site...",
+        image: "🖼️",
+        description: "Site Description will go here!"
+    }
+]
