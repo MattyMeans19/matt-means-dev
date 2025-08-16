4ad527eecc8171c64695f8ea2ae587bee9758ad9
Matt <mattymeans@gmail.com>
2025-08-16 01:18:38 -0600

    update to descriptions

--- src/Sites.js
+++ src/Sites.js
@@ -14,7 +14,7 @@ export const sites = [
         name: "Small Business Template",
         url: "https://smallbusiness-p-e6a955b1be25.herokuapp.com/",
         image: SBTImage,
-        description: "This site is a template for a Small Business web page. It's intended to provide a live inventory and cart reservations for customers, while also providing information on the business. It was built using React and TailwindCSS for the font end, and Express.js for the backend using a custom API for a PostgreSQL database I built. If you add 'business-portal' to the base URL it will take you to a business-side dashboard for inventory and user management. you can check out all the code at my Github Repo: https://github.com/MattyMeans19/small-business-premium"
+        description: "This site is a template for a Small Business web page. It's intended to provide a live inventory and cart reservations for customers, while also providing information on the business. It was built using React and TailwindCSS for the font end, and Express.js for the backend using a custom API for a PostgreSQL database I built. If you add 'business-portal' to the base URL it will take you to a business-side dashboard for inventory and user management and you can login as USername:JDOE using the password:Password! . You can check out all the code at my Github Repo: https://github.com/MattyMeans19/small-business-premium"
     },
     {
         id: 3, 
