77838251ea0a18abfbb2baa8893c03d4eb481647
Matt <mattymeans@gmail.com>
2025-08-16 01:03:43 -0600

    Added Site 2

--- src/Sites.js
+++ src/Sites.js
@@ -1,4 +1,5 @@
 import AKSImage from "/allkeyboardshortcuts.png";
+import SBTImage from "/SBtemplate.png";
 
 export const sites = [
     {
@@ -10,10 +11,10 @@ export const sites = [
     },
     {
         id: 2, 
-        name: "Site 2",
-        url: "URL for site...",
-        image: "🖼️",
-        description: "Site Description will go here!"
+        name: "Small Business Template",
+        url: "https://smallbusiness-p-e6a955b1be25.herokuapp.com/",
+        image: SBTImage,
+        description: "This site is a template for a Small Business web page. It's intended to provide a live inventory and cart reservations for customers, while also providing information on the business. It was built using React and TailwindCSS for the font end, and Express.js for the backend using a custom API for a PostgreSQL database I built. If you add 'business-portal' to the base URL it will take you to a business-side dashboard for inventory and user management. you can check out all the code at my Github Repo: https://github.com/MattyMeans19/small-business-premium"
     },
     {
         id: 3, 
