3f867af436b11618e9f3422ce31ee257896f49b8
Matt <mattymeans@gmail.com>
2025-08-20 21:50:48 -0600

    Small edit

--- src/Sites.js
+++ src/Sites.js
@@ -7,14 +7,14 @@ export const sites = [
         name: "All Keyboard Shortcuts",
         url: "https://allkeyboardshortcuts.info",
         image: AKSImage,
-        description: "A simple webiste that contains lists of keyboard shortcuts for various Operating Systems and Applications!\nIt was built using React and TailwindCSS, you can check out all the code at my Github Repo: https://github.com/MattyMeans19/all-keyboard-shortcuts"
+        description: "A simple webiste that contains lists of keyboard shortcuts for various Operating Systems and Applications!\nIt was built using React + Vite and TailwindCSS, you can check out all the code at my Github Repo: https://github.com/MattyMeans19/all-keyboard-shortcuts"
     },
     {
         id: 2, 
         name: "Small Business Template",
         url: "https://smallbusiness-p-e6a955b1be25.herokuapp.com/",
         image: SBTImage,
-        description: "This site is a template for a Small Business web page. It's intended to provide a live inventory and cart reservations for customers, while also providing information on the business. It was built using React and TailwindCSS for the font end, and Express.js for the backend using a custom API for a PostgreSQL database I built. If you add 'business-portal' to the base URL it will take you to a business-side dashboard for inventory and user management and you can login using 'Admin' for the username and password. You can check out all the code at my Github Repo: https://github.com/MattyMeans19/small-business-premium"
+        description: "This site is a template for a Small Business web page. It's intended to provide a live inventory and cart reservations for customers, while also providing information on the business. It was built using React + Vite and TailwindCSS for the font end, and Express.js for the backend using a custom API for a PostgreSQL database I built. If you add 'business-portal' to the base URL it will take you to a business-side dashboard for inventory and user management and you can login using 'Admin' for the username and password. You can check out all the code at my Github Repo: https://github.com/MattyMeans19/small-business-premium"
     },
     {
         id: 3, 
