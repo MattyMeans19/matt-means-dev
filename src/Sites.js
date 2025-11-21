08b3cfffe102b2c56bb1279f9ae3e59e17440ecf
Matt <mattymeans@gmail.com>
2025-11-21 01:02:21 -0700

    Fix to AKS repo URL

--- src/Sites.js
+++ src/Sites.js
@@ -8,7 +8,7 @@ export const sites = [
         name: "All Keyboard Shortcuts",
         url: "https://allkeyboardshortcuts.info",
         image: AKSImage,
-        description: "A simple webiste that contains lists of keyboard shortcuts for various Operating Systems and Applications!\nIt was built using React + Vite and TailwindCSS, you can check out all the code at my Github Repo: https://github.com/MattyMeans19/all-keyboard-shortcuts"
+        description: "A simple webiste that contains lists of keyboard shortcuts for various Operating Systems and Applications!\nIt was built using React + Vite and TailwindCSS, you can check out all the code at my Github Repo: https://github.com/MattyMeans19/all-keyboard-shortcuts.git"
     },
     {
         id: 2, 
